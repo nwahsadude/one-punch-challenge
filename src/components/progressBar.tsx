6b685759d2be53d563c9f6eee88e2493ed99d23a
nwahsadude <nwahsadude@gmail.com>
2023-03-02 15:27:48 -0600

    change color on progress bar

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -6,7 +6,7 @@ function ProgressBar({ progress, goal }: PropsWithoutRef<{ workout: string, prog
 
   return (
       <div className="w-full bg-gray-200 rounded-full h-6 dark:bg-gray-700">
-        <div style={{ width: `${percentage}%` }} className="h-6 bg-blue-600 rounded-full dark:bg-blue-700 text-center font-medium text-blue-100">{percentage}%</div>
+        <div style={{ width: `${percentage}%` }} className="h-6 bg-red-600 rounded-full dark:bg-red-700 text-center font-medium text-red-100">{percentage}%</div>
       </div>
   );
 }
