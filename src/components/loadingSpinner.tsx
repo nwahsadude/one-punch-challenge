fb18dc35869e56dd85c17b4f15536140e7e0b8dc
nwahsadude <nwahsadude@gmail.com>
2023-03-03 11:56:58 -0600

    switched chart to react-charts

--- src/components/loadingSpinner.tsx
+++ src/components/loadingSpinner.tsx
@@ -1,15 +1,17 @@
-import React, { PropsWithoutRef } from "react";
+import React, { type PropsWithoutRef } from "react";
 
-function LoadingSpinner({ loadingText = "Loading" }: PropsWithoutRef<{ loadingText?: string }>) {
+function LoadingSpinner({
+  loadingText = "Loading",
+}: PropsWithoutRef<{ loadingText?: string }>) {
   return (
-    <div className="flex flex-col justify-center place-items-center pt-52">
+    <div className="flex flex-col place-items-center justify-center">
       <div
         className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-red-600 motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status">
-    <span
-      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-    >Loading...</span
-    >
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
       </div>
       <span className="text-xl">{loadingText}...</span>
     </div>
--- src/components/progressChart.tsx
+++ src/components/progressChart.tsx
@@ -1,57 +1,103 @@
-import React, { type PropsWithoutRef } from "react";
+import React, { type PropsWithoutRef, useMemo } from "react";
 import { api } from "~/utils/api";
-import { VictoryBar, VictoryChart, VictoryGroup, VictoryTooltip } from "victory";
 import { type WorkoutSession } from ".prisma/client";
 import LoadingSpinner from "~/components/loadingSpinner";
+import dynamic from "next/dynamic";
+import { type AxisOptions } from "react-charts";
+
+const Chart = dynamic(() => import("react-charts").then((mod) => mod.Chart), {
+  ssr: false,
+});
+interface DataPoint {
+  name: string;
+  amount: number;
+  userId: string;
+  date: string;
+}
+
+interface DataSeries {
+  label: string;
+  data: DataPoint[];
+}
 
 function ProgressChart({ workout }: PropsWithoutRef<{ workout: string }>) {
-
   const { data } = api.workoutSession.getOthersSessions.useQuery(workout);
 
-  if (!data) return (<LoadingSpinner />);
+  const primaryAxis = React.useMemo<AxisOptions<any>>(
+    () => ({
+      getValue: (datum: DataPoint) => datum.date,
+      showGrid: false,
+      innerBandPadding: 0.3,
+      innerSeriesBandPadding: 0.05,
+    }),
+    []
+  );
 
+  const secondaryAxes = useMemo<AxisOptions<any>[]>(() => {
+    return [
+      {
+        getValue: (datum: DataPoint) => datum.amount,
+        hardMin: 0,
+        hardMax: 100,
+        showGrid: false,
+        elementType: "bar",
+      },
+    ];
+  }, []);
 
-  const prepData = (data: (WorkoutSession & { user: { name: string | null } })[]) => {
+  const prepData = (
+    data: (WorkoutSession & { user: { name: string | null } })[]
+  ) => {
+    const users = [...new Set(data.map((d) => d.user.name))];
 
-    return data.reduce((acc, cur) => {
-      const date = cur.dateTimeCreated.toLocaleDateString();
-      const existingTotalAmount = acc.find(total => total.userId === cur.userId && total.date === date);
+    return users.reduce<DataSeries[]>((dataseries, name) => {
+      return [
+        ...dataseries,
+        {
+          label: name ? name : "",
+          data: data
+            .filter((r) => r.user.name === name)
+            .reduce<DataPoint[]>((acc, cur) => {
+              const date = cur.dateTimeCreated.toLocaleDateString();
+              const existingTotalAmount = acc.find(
+                (total) => total.userId === cur.userId && total.date === date
+              );
 
-      if (existingTotalAmount) {
-        existingTotalAmount.totalAmount += cur.amount;
-      } else {
-        acc.push({
-          userId: cur.userId,
-          label: `${cur.user.name || ""} - ${cur.amount}`,
-          user: cur.user.name || "",
-          date: date,
-          totalAmount: cur.amount
-        });
-      }
-
-      return acc;
-    }, [] as { userId: string, user: string, label: string, date: string, totalAmount: number }[]);
-
+              if (existingTotalAmount) {
+                existingTotalAmount.amount += cur.amount;
+              } else {
+                acc.push({
+                  userId: cur.userId,
+                  name: cur.user.name || "",
+                  amount: cur.amount,
+                  date: date,
+                });
+              }
+              return acc;
+            }, []),
+        },
+      ];
+    }, []);
   };
+
+  if (!data)
+    return (
+      <div className="pt-24">
+        <LoadingSpinner />
+      </div>
+    );
 
   const chartData = prepData(data);
 
   return (
-    <div>
-      <VictoryChart>
-        <VictoryGroup>
-
-          {
-            chartData &&
-            chartData.map((d) => {
-              return (
-                <VictoryBar labelComponent={<VictoryTooltip />} key={d.user} data={chartData}
-                            x={"date"} y={"totalAmount"} />);
-            })
-
-          }
-        </VictoryGroup>
-      </VictoryChart>
+    <div className="block h-40 w-full pt-5">
+      <Chart
+        options={{
+          data: chartData,
+          primaryAxis,
+          secondaryAxes,
+        }}
+      />
     </div>
   );
 }
