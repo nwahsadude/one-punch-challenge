31829627076a48332bc3648cdfab9d8f4ebfbf12
nwahsadude <nwahsadude@gmail.com>
2023-03-14 16:48:39 -0500

    Refactor welcome page with new background image and login buttons

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,56 +1,61 @@
 import React, { useEffect } from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { Bangers } from "@next/font/google";
 import { useRouter } from "next/navigation";
 
 const bangers = Bangers({ subsets: ["latin"], weight: "400" });
 
 function WelcomePage() {
+  const { status: sessionStatus } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (sessionStatus === "authenticated") {
+      router.push("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sessionStatus]);
+
   return (
-    <div className={bangers.className}>
-      <div className="h-screen bg-gray-600">
-        <div className="flex flex-col items-center text-white tracking-wider">
-          <h1>
+    <div
+      className={bangers.className}
+      style={{
+        backgroundImage: "url(/background.jpg)",
+        width: "100%",
+        height: "100%",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-auto w-1/2 rounded-lg bg-gray-700 p-10 shadow-md">
+          <h2 className="mb-6 text-center text-2xl font-bold tracking-wider text-white">
             Welcome to the Greatest Workout Routine in the World!
-          </h1>
-          <p>100 situps, pushups, and squats followed by a 6.2-mile (10-km) run.</p>
-          <p>
-            Set your goal of a percentage of the total workout and work your way up
-            to developing the strength to defeat any thing with a single punch.
+          </h2>
+          <p className="mb-6 tracking-wider text-white">
+            100 situps, pushups, and squats followed by a 6.2-mile (10-km) run.
+            Set your goal of a percentage of the total workout and work your way
+            up to developing the strength to defeat any thing with a single
+            punch.
           </p>
+          <div className="flex justify-center gap-4">
+            <button
+              className="rounded-md bg-blue-600 py-2 px-4 text-white shadow-md transition duration-200 hover:bg-blue-700"
+              onClick={() => void signIn("discord")}
+            >
+              Login with Discord
+            </button>
+            <button
+              className="rounded-md bg-red-600 py-2 px-4 text-white shadow-md transition duration-200 hover:bg-red-700"
+              onClick={() => void signIn("google")}
+            >
+              Login with Google
+            </button>
+          </div>
         </div>
-        <AuthShowcase />
       </div>
     </div>
   );
 }
 
 export default WelcomePage;
-
-
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData, status: sessionStatus  } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (sessionStatus === 'authenticated') {
-      router.push("/");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sessionStatus]);
-
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
