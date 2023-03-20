95e50be75c84ae299e4752c235c57bd6b4d7344e
nwahsadude <nwahsadude@gmail.com>
2023-03-20 08:31:12 -0500

    Display 0% if no workouts recorded.

--- src/components/dailyProgress.tsx
+++ src/components/dailyProgress.tsx
@@ -3,37 +3,39 @@ import ProgressBar from "~/components/progressBar";
 import { api } from "~/utils/api";
 import LoadingSpinner from "~/components/loadingSpinner";
 
-function DailyProgress({
-                         workouts
-                       }: PropsWithoutRef<{ workouts: string[] }>) {
+function DailyProgress({ workouts }: PropsWithoutRef<{ workouts: string[] }>) {
+  const { data: workoutSessions } =
+    api.workoutSession.getTodaySessions.useQuery();
 
-  const { data: workoutSessions } = api.workoutSession.getTodaySessions.useQuery();
-
-  if (!workoutSessions) return (<LoadingSpinner loadingText={"Loading progress"} />);
+  if (!workoutSessions)
+    return <LoadingSpinner loadingText={"Loading progress"} />;
 
   function getProgress(workoutType: string) {
     if (workoutSessions) {
-      return workoutSessions.filter((session) => session.workoutType === workoutType)
-        .reduce((acc: number, cur) => (acc + cur.amount), 0);
-
+      return workoutSessions
+        .filter((session) => session.workoutType === workoutType)
+        .reduce((acc: number, cur) => acc + cur.amount, 0);
     }
     return 0;
-
   }
 
   return (
     <div>
-      <h1 className="text-center text-2xl bg-gray-600 border-black border-b-2 py-2 text-white">Daily Progress</h1>
+      <h1 className="border-b-2 border-black bg-gray-600 py-2 text-center text-2xl text-white">
+        Daily Progress
+      </h1>
 
       <div className="px-4">
-
-      {workouts.map(workout => (
-        <div key={workout} className="border-black border-b-2 pt-2 pb-2">
-          <h1 className="text-2xl text-center pb-1 text-white">{workout}</h1>
-          {workoutSessions.length && <ProgressBar workout={workout} progress={getProgress(workout)} goal={100} />}
-
-        </div>
-      ))}
+        {workouts.map((workout) => (
+          <div key={workout} className="border-b-2 border-black pt-2 pb-2">
+            <h1 className="pb-1 text-center text-2xl text-white">{workout}</h1>
+            <ProgressBar
+              workout={workout}
+              progress={getProgress(workout)}
+              goal={100}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,13 +1,20 @@
 import React, { type PropsWithoutRef } from "react";
 
-function ProgressBar({ progress, goal }: PropsWithoutRef<{ workout: string, progress: number, goal: number }>) {
-
+function ProgressBar({
+  progress,
+  goal,
+}: PropsWithoutRef<{ workout: string; progress: number; goal: number }>) {
   const percentage = Math.floor((progress / goal) * 100);
-
+  console.log(percentage)
   return (
-      <div className="w-full bg-gray-200 rounded-full h-6 dark:bg-gray-700">
-        <div style={{ width: `${percentage}%` }} className="h-6 bg-red-600 rounded-full dark:bg-red-700 text-center font-medium text-red-100">{percentage}%</div>
+    <div className="h-6 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        style={{ width: `${percentage}%` }}
+        className="h-6 rounded-full bg-red-600 text-center font-medium text-red-100 dark:bg-red-700"
+      >
+        {percentage}%
       </div>
+    </div>
   );
 }
 
